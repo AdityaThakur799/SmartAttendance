477c9375dcd9c23ae5a0c41a062f1eee92124291
AdityaThakur799 <adityathakur9348@gmail.com>
2026-08-21 00:22:51 +0530

    Fix Railway PORT config and clean up old MySQL dependency

--- src/main/java/com/smartattendance/SmartAttendanceApplication.java
+++ src/main/java/com/smartattendance/SmartAttendanceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class SmartAttendanceApplication {
+public class  SmartAttendanceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SmartAttendanceApplication.class, args);
